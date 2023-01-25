01bd2643a2325812fff33335626a573e69c9d068
www-net <78052177+www-net@users.noreply.github.com>
2023-01-25 21:28:15 +0300

    added map

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,7 @@
-var link = document.querySelector(".js-write_us-button");
-var popup = document.querySelector(".modal-write_us");
-var login = popup.querySelector("[name=login]")
-var close = document.querySelector(".modal-close");
+const link = document.querySelector(".js-write_us-button");
+const popup = document.querySelector(".modal-write_us");
+const login = popup.querySelector("[name=login]");
+const close = document.querySelector(".modal-close");
 
 link.addEventListener("click", function(evt) {
     evt.preventDefault();
@@ -15,7 +15,7 @@ close.addEventListener("click", function(evt) {
 });
 
 window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
+    if (evt.key == "Escape") {
         if (popup.classList.contains("modal-show")) {
             evt.preventDefault();
             popup.classList.remove("modal-show");
